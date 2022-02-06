c271d9b9ba090469d1171cc0eecc73e91ad8e193
Tutorialonly <98994880+Tutorialonly@users.noreply.github.com>
2022-02-07 06:21:11 +0700

    Add files via upload

--- source/Playstate.hx
+++ source/Playstate.hx
@@ -70,15 +70,15 @@ class PlayState extends MusicBeatState
 
 	public static var ratingStuff:Array<Dynamic> = [
 		['You Suck!', 0.2], //From 0% to 19%
-		['SUPER NOOB 100%', 0.4], //From 20% to 39%
-		['very very very noob', 0.5], //From 40% to 49%
-		['Very noob, 0.6], //From 50% to 59%
-		['Noob', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
+		['Shit', 0.4], //From 20% to 39%
+		['Bad', 0.5], //From 40% to 49%
+		['Bruh', 0.6], //From 50% to 59%
+		['Noob, 0.69], //From 60% to 68%
+		['Lol', 0.7], //69%
 		['Not bad', 0.8], //From 70% to 79%
-		['umm.its ok', 0.9], //From 80% to 89%
-		['Pro', 1], //From 90% to 99%
-		['HACKER!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Nice', 0.9], //From 80% to 89%
+		['Good!', 1], //From 90% to 99%
+		['Super pro!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	#if (haxe >= "4.0.0")
@@ -913,7 +913,7 @@ class PlayState extends MusicBeatState
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
-		add(Hacker!!Txt);
+		add(botplayTxt);
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
