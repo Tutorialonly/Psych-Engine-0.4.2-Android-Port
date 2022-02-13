4c593125c59ca19f016f05ca1f1f1b60e939c891
Tutorialonly <98994880+Tutorialonly@users.noreply.github.com>
2022-02-13 17:50:39 +0700

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -109,7 +109,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 		char1 = new Character(800, -130, 'bf', true);
-		char1.SetGraphicSize(Std.int(Char1.width = 0.8));
+		char1.setGraphicSize(Std.int(Char1.width = 0.8));
 		add(char1);
 		char1.visible = false;
 
