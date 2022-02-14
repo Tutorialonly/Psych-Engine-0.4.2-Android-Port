b12128ba6c1941c3d2018307a7acb91ff6c5502f
Tutorialonly <98994880+Tutorialonly@users.noreply.github.com>
2022-02-14 18:54:59 +0700

    Fix main menu state

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -36,9 +36,7 @@ class MainMenuState extends MusicBeatState
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
-    private var char1:Character =null;
-    
-    
+
 	override function create()
 	{
 		#if desktop
@@ -96,8 +94,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			//menuItem.screenCenter(X);
-			menuItem.x += 0;
+			menuItem.screenCenter(X);
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
@@ -108,13 +105,6 @@ class MainMenuState extends MusicBeatState
 		}
 
 		FlxG.camera.follow(camFollowPos, null, 1);
-		
-		
-		char1 = new Character(800, -130, 'bf', true);
-		char1.setGraphicSize(Std.int(char1.width = 0.8));
-		add(char1);
-		char1.visible = false;
-
 
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
@@ -170,19 +160,6 @@ class MainMenuState extends MusicBeatState
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 5.6, 0, 1);
 		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
 
-         if (optionShit[curSelected] == 'story_mode,Freeplay')
-         {
-              changeItem(-1);
-              changeItem(1);
-              
-              char1.dance();
-              char1.updateHitbox();
-              char1.visible = true;
-         }
-             else
-          {
-               char1.visible = false;
-          }
 		if (!selectedSomethin)
 		{
 			if (controls.UI_UP_P)
@@ -264,7 +241,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			//spr.screenCenter(X);
+			spr.screenCenter(X);
 		});
 	}
 
